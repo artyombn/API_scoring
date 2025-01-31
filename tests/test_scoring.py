7296951796f62ddf766fd2b4238367d8ea035791
artyombn <balabashinan@gmail.com>
2025-01-31 19:00:15 +0300

    test scoring done

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -1,6 +1,80 @@
 import pytest
+from contextlib import nullcontext as does_not_raise
 
 from scoring import get_score, get_interests
 
-def test_get_score():
-    pass
+
+@pytest.mark.parametrize(
+    "arguments, score",
+    [
+        (
+                {"store": 1,
+                 "phone": "79123456789",
+                 "email": "user@gmail.com",
+                 "birthday": "01.01.1996",
+                 "gender": 1,
+                 "first_name": "Alex",
+                 "last_name": "Stepnov"
+                 }, 5),
+        (
+                {"store": 1,
+                 "email": "user@gmail.com",
+                 "birthday": "01.01.1996",
+                 "gender": 1,
+                 "first_name": "Alex",
+                 "last_name": "Stepnov"
+                 }, 3.5),
+        (
+                {"store": 1,
+                 "phone": "79123456789",
+                 "birthday": "01.01.1996",
+                 "gender": 1,
+                 "first_name": "Alex",
+                 "last_name": "Stepnov"
+                 }, 3.5),
+        (
+                {"store": 1,
+                 "phone": "79123456789",
+                 "email": "user@gmail.com",
+                 "birthday": "01.01.1996",
+                 "first_name": "Alex",
+                 "last_name": "Stepnov"
+                 }, 3.5),
+        (
+                {"store": 1,
+                 "phone": "79123456789",
+                 "email": "user@gmail.com",
+                 "birthday": "01.01.1996",
+                 "gender": 1,
+                 "last_name": "Stepnov"
+                 }, 4.5),
+        (
+                {"store": 1,
+                 "birthday": "01.01.1996",
+                 "gender": 1,
+                 "first_name": "Alex",
+                 "last_name": "Stepnov"
+                 }, 2),
+        (
+                {"store": 1,
+                 "phone": "79123456789",
+                 }, 1.5),
+        (
+                {"store": 1,
+                 "email": "user@gmail.com",
+                 }, 1.5),
+    ]
+)
+def test_get_score(arguments, score):
+    if "phone" not in arguments.keys():
+        arguments["phone"] = None
+    if "email" not in arguments.keys():
+        arguments["email"] = None
+    assert get_score(**arguments) == score
+
+
+def test_get_interests():
+    data = ["cars", "pets", "travel", "hi-tech", "sport", "music", "books", "tv", "cinema", "geek", "otus"]
+    assert isinstance(get_interests(None, None), list)
+    assert len(get_interests(None, None)) == 2
+    assert all(item in data for item in get_interests(None, None))
